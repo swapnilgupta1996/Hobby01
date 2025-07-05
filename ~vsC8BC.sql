731c3f6ca30b7be89eafdd365272e8687dca183d
SWAPNILGUPTA <swapnil.gupta@somaiya.edu>
2025-07-05 13:03:53 -0500

    Update ~vsC8BC.sql

--- ~vsC8BC.sql
+++ ~vsC8BC.sql
@@ -43,3 +43,46 @@ WHERE [Group] LIKE 'NORTH AMERICA'
 
 
 SELECT * FROM NASalesQuota2
+
+-------------------------------------
+
+--- Triggers 
+
+SELECT * FROM AdventureWorks2012.HumanResources.Shift
+
+
+SELECT * FROM HumanResources.Shift
+create trigger demotrigger
+ON HumanResources.shift
+AFTER INSERT 
+AS
+BEGIN
+PRINT 'INSERT IS NOT ALLOWED. YOU NEED APPROVAL'
+ROLLBACK TRANSACTION
+END 
+GO
+
+-- testing the trigger 
+insert into HumanResources.Shift
+([Name],[StartTime],[EndTime],[ModifiedDate])
+VALUES
+('Rakesh','08:00:00.0000000','06:00:00.0000000', GETDATE ())
+
+-- getting the date to check  the date syntax
+SELECT GETDATE ()
+
+
+---Database level trigger 
+
+Create trigger demodbtrigger 
+on DATABASE 
+AFTER CREATE_TABLE
+AS
+BEGIN
+PRINT 'CREATION OF NEW TABLES NOT ALLOWED'
+END
+GO
+
+---tRYING TO CREATE A TABLE 
+
+CREATE TABLE MYDEMOTABLE(col1 varchar(10))
